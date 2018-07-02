f68f11c08e53b3ce28882aebe5d6cf3d7fca040a
karinafigueira <karinaottofigueira@gmail.com>
2018-07-02 19:46:58 -0300

    included Selenium.Utils

---
+++ democart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace democart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ democart/Selenium.Utils/Browser.cs
@@ -0,0 +1,10 @@
+namespace Selenium.Utils
+{
+    public enum Browser
+    {
+        Firefox,
+        Chrome,
+        InternetExplorer,
+        Edge
+    }
+}
